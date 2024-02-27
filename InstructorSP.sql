35457adcffe94f43beca8a9ec8452b6504858a36
Esraa Mohamed <124937321+ScarlAsh@users.noreply.github.com>
2024-02-27 19:13:17 +0200

    Update InstructorSP.sql

--- InstructorSP.sql
+++ InstructorSP.sql
@@ -2,9 +2,9 @@ Alter proc getCoursesByInstructor @Ins_Id int
 as
   select c.*
   from Course c,Instructor i
-  where c.ins_id = i.ins_id and i.ins_id = @Ins_Id
+  where c.ins_id = i.Id and i.Id = @Ins_Id
 
-
+GO
 create proc getExamsofCourse @crs_id int
 as
   select e.*
@@ -12,16 +12,16 @@ as
   where Course_id = crs_id and Course_id = @crs_id
 
 
-
-create proc getStudentInSpecificCourse @course_Id int
+GO
+alter proc getStudentInSpecificCourse @course_Id int
 as
   select s.*
   from Student s , StudentCourses sc , Course c
-  where s.Std_id = sc.Std_id and sc.Course_id = c.Course_id and c.Course_id = @course_Id
+  where s.Id = sc.Std_id and sc.Course_id = c.Course_id and c.Course_id = @course_Id
 
 
 
-
+GO
 alter PROCEDURE sp_UpdateExam
     @examID int,
 	@duration int, 
