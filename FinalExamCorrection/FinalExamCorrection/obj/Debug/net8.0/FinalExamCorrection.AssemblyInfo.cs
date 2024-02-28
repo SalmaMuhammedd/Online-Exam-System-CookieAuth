ed8c1b1aa6742fdcfcb2a411afcacd94fc4e10bc
SalmaMuhammedd <salmamuhammed2112@gmail.com>
2024-02-28 16:40:56 +0200

    modified submit exam: to submit not fully answered exam, modified GenerateExam.cshtml, NumOfMCQ & NumOfTF

--- FinalExamCorrection/FinalExamCorrection/obj/Debug/net8.0/FinalExamCorrection.AssemblyInfo.cs
+++ FinalExamCorrection/FinalExamCorrection/obj/Debug/net8.0/FinalExamCorrection.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalExamCorrection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62391fa498b3207070c3477747dc399674922bbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ff74eda9b85a2179602e9f0c78e011bcff29e1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalExamCorrection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalExamCorrection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
